0ba5622fabac8d6b699ec664845e08b9bf9f3e68
wjschr18 <wjschr18@stlawu.edu>
2020-04-29 14:29:24 -0400

    require(teams)

--- models/user.js
+++ models/user.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const Team = require('./models/team')
 
 const User = new mongoose.Schema({
   _id: String,
-  teams: Team
+  teams: [Team]
 });
 
 module.exports = mongoose.model('User', User);
