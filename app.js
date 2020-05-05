cdac87220c6fef9ffa79f4befe24c1e90f2b2405
Riley Cott <rcott@Rileys-MacBook-Pro-3.local>
2020-05-05 15:38:01 -0400

    login

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(function(request, response, next) {
 });
 
 app.use(function(request, response, next) {
-  response.locals.user = request.session.user;
+  response.locals.User = request.session.User;
   next();
 });
 
