69293670b8b9cac98d5e3932db88beda4dc648b3
wjschr18 <wjschr18@stlawu.edu>
2020-05-06 13:51:49 -0400

    janky

--- router.js
+++ router.js
@@ -20,8 +20,7 @@ router.get('/logout', function(request, response) {
 });
 
 router.get('/teams/new', function(request, response){
-  Team.find().then(teams => response.render('newTeam', {teams: teams}));
-  User.find().then(users => response.render('newTeam', {users: users}));
+  Team.find().then(teams => User.find().then(users => response.render('newTeam', {teams: teams}, {users: users})):
 });
 
 router.get('/teams', function(request, response){
