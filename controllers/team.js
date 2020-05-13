b5cbe0986c9a159e8f55415d797251cdb0920578
Riley Cott <rcott@Rileys-MacBook-Pro-3.local>
2020-05-13 14:25:57 -0400

    scores

--- controllers/team.js
+++ controllers/team.js
@@ -10,6 +10,7 @@ module.exports.create = function(request, response, next) {
 
 // PUT /team/:id (with the changes in the request body)
 module.exports.update = function(request, response, next) {
+  
   Team.findByIdAndUpdate(request.params.id, request.body)
     .then(team => team ? response.status(200).end() : next())
     .catch(error => next(error));
