375ac85904bee3010470724b9010221848679da7
Riley Cott <rcott@Rileys-MacBook-Pro-3.local>
2020-05-14 16:58:56 -0400

    final

--- controllers/users.js
+++ controllers/users.js
@@ -13,14 +13,6 @@ module.exports.login = function(request, response, next) {
 };
 
 module.exports.create = function(request, response, next) {
-  User.findById(request.body._id)
-    .then(function(user) {
-      if (request.body === user._id) {
-        response.status(400).end();
-    } else {
-      next();
-    }
-  }).catch(error => next(error));
   User.create(request.body)
     .then(user => response.status(201).send(user._id))
     .catch(error => next(error));
